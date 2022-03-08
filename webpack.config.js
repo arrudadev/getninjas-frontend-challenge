101a738b10a66419e9efc071fd6f61903316a58b
Alexandre <alexandre.monteiro.bec@gmail.com>
2022-03-08 14:24:32 -0300

    chore: setup eslint and prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,52 @@
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
-
 module.exports = {
-    entry: './src/entry.ts',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            { 
-                test: /\.css$/, 
-                use: [MiniCssExtractPlugin.loader, 'css-loader'] 
-            },
-            {
-                test: /\.html$/i,
-                loader: "html-loader",
-              },
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    optimization: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerWebpackPlugin(),
-            '...'
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-            hash: true
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style.css'
-        }),
-        new webpack.optimize.ModuleConcatenationPlugin()
+  entry: './src/entry.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
     ],
-    devServer: {        
-        port: 3000
-    }
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new CssMinimizerWebpackPlugin(), '...'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      hash: true,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+  ],
+  devServer: {
+    port: 3000,
+  },
 };
