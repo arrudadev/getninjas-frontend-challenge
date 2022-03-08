72f5b0590f78becbc50e362667ee365a9ddb490a
Alexandre <alexandre.monteiro.bec@gmail.com>
2022-03-08 13:06:20 -0300

    chore: setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin');
+
+module.exports = {
+    entry: './src/entry.ts',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            { 
+                test: /\.css$/, 
+                use: [MiniCssExtractPlugin.loader, 'css-loader'] 
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+              },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerWebpackPlugin(),
+            '...'
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            hash: true
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin()
+    ],
+    devServer: {        
+        port: 3000
+    }
+};
