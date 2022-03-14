7d0cd0e974138f04edb059cbbb8a24caa8483379
Alexandre <alexandre.monteiro.bec@gmail.com>
2022-03-14 00:59:24 -0300

    chore: exclude api folder in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
       {
         test: /\.ts?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
+        exclude: /(node_modules|api)/,
       },
       {
         test: /\.css$/,
